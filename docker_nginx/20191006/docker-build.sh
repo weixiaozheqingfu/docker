4bc056a9558c37b20676048dcc8a381e8f58dea9
limengjun <limengjun@syswin.com>
2019-10-09 10:09:22 +0800

    增加nginx镜像

---
+++ docker_nginx/20191006/docker-build.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+IMAGE_NAME=docker_image_nginx:1.17.2:20191006
+
+echo $IMAGE_NAME
+
+docker build -t $IMAGE_NAME ./
+
+sleep 6s
+
+docker tag $IMAGE_NAME  weixiaozheqingfu/$IMAGE_NAME
+
+sleep 6s
+
+# docker push  weixiaozheqingfu/$IMAGE_NAME
+
+echo "创建镜像完成"
+echo 镜像路径：weixiaozheqingfu/$IMAGE_NAME
---
+++ docker_nginx/20191006/docker-run.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+FILE_NAME=docker_image_nginx:1.17.2
+IMAGE_VERSION=20191006
+IMAGE_NAME=weixiaozheqingfu/$FILE_NAME:$IMAGE_VERSION
+
+docker rm $FILE_NAME
+docker run --name $FILE_NAME -p 80:80 -d $IMAGE_NAME
+
+
+# 参考
+# docker run -d -p 8021:8081 -p 8766:8766 --add-host disconf.systoon.com:172.28.18.48 reg1.syswin.com/phenix/org-contact:20190709044300
