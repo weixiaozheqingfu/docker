db021c73d5a39ecd412a6b73603d8ad6007133a9
limengjun <limengjun@syswin.com>
2019-10-18 11:35:14 +0800

    增加docker_disconf

---
+++ docker_disconf/20191015/mysql/docker-run.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+FILE_NAME=docker_image_mysql_5.7.27
+IMAGE_VERSION=20191015
+IMAGE_NAME=weixiaozheqingfu/$FILE_NAME:$IMAGE_VERSION
+CONTAINER_NAME=disconf_mysql
+
+docker rm $FILE_NAME
+docker run --name $CONTAINER_NAME -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=root $IMAGE_NAME
+
+# 参考
+# docker run -d -p 8021:8081 -p 8766:8766 --add-host disconf.systoon.com:172.28.18.48 reg1.syswin.com/phenix/org-contact:20190709044300
---
+++ docker_tomcat8/20191018/docker-run.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+FILE_NAME=docker_image_tomcat_8.5.46
+IMAGE_VERSION=20191018
+IMAGE_NAME=weixiaozheqingfu/$FILE_NAME:$IMAGE_VERSION
+
+docker rm $FILE_NAME
+docker run --name $FILE_NAME -p 8080:8080 -d --add-host disconf.systoon.com:172.28.18.48 $IMAGE_NAME 
+
+
+# 参考
+# docker run -d -p 8021:8081 -p 8766:8766 --add-host disconf.systoon.com:172.28.18.48 reg1.syswin.com/phenix/org-contact:20190709044300
