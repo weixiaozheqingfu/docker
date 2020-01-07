067ab05de7a32887cf9fbab8100632770c3ceac4
limengjun <limengjun@syswin.com>
2020-01-07 10:06:30 +0800

    增加mysql主从复制

---
+++ docker_mysql-master-slave/20200106/docker-build/slave/docker-build.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+IMAGE_NAME=mysql_slave:20200106
+
+echo $IMAGE_NAME
+
+docker build -t $IMAGE_NAME ./
+
+sleep 6s
+
+docker tag $IMAGE_NAME  weixiaozheqingfu/$IMAGE_NAME
+
+sleep 6s
+
+# docker push  weixiaozheqingfu/$IMAGE_NAME
+
+echo "创建镜像完成"
+echo 镜像路径：weixiaozheqingfu/$IMAGE_NAME
